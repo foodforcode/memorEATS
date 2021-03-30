d20b111d37a03c7ea88129d30bdcc0b30405d1cd
foodforcode <tsai.connie@yahoo.com>
2021-03-30 02:06:45 -0700

    Add body-parser

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
+const db = require('../database/');
+
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../client/dist/')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// app.get('/*', (req, res) => {
-//   res.send('hello world');
-// });
+app.use(express.static(path.join(__dirname, '../client/dist/')));
 
 app.listen(PORT, () => {
   console.log(`Snacking on port ${PORT}...`);
