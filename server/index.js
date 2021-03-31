4489fa214462a37fe8e566832a9c5dcd85959be0
foodforcode <tsai.connie@yahoo.com>
2021-03-31 01:57:11 -0700

    Add handlers for /posts and /post

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
 const db = require('../database/');
+const addNewPost = require('../database/controllers/addNewPost.js');
+const getAllPosts = require('../database/controllers/getAllPosts.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +12,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, '../client/dist/')));
+
+app.get('/posts', (req, res) => {
+
+})
+
+
+app.post('/post', (req, res) => {
+  addNewPost(req.body, (err, data) => {
+    if(err) {
+      console.log(err);
+    } else {
+      res.status(201).send(data);
+    }
+  })
+});
 
 app.listen(PORT, () => {
   console.log(`Snacking on port ${PORT}...`);
