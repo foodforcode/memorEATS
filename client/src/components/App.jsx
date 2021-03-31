4712a6146a607ed96197fd7d35fd4c9533139a0d
foodforcode <tsai.connie@yahoo.com>
2021-03-31 01:54:17 -0700

    Update components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,34 @@
 import React from 'react';
 import Form from './Form.jsx';
+import PostContainer from './PostContainer.jsx';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      location: '',
-      body: '',
+      posts: [],
 
     }
 
+  }
+
+
+  componentDidMount() {
+    axios.get('/posts')
+      .then((response) => {
+        this.setState({
+          posts: response.data
+        })
+      })
   }
 
   render() {
     return(
       <div>
         <Form />
+        <PostContainer />
       </div>
     )
   }
---
+++ client/src/components/PostContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PostContainer (props) {
+  return (
+    <h1>Here</h1>
+  )
+}
+
+export default PostContainer;
