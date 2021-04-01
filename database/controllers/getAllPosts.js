2646add7367c4d5d70380a5e97499a09adf33f47
foodforcode <tsai.connie@yahoo.com>
2021-04-01 02:38:29 -0700

    Add controller to get all posts

--- database/controllers/getAllPosts.js
+++ database/controllers/getAllPosts.js
@@ -1,8 +1,13 @@
 const Post = require('../models/Post.js');
 
-const getAllPosts = (req, res) => {
-  var results = Post.find({});
-  res.send(results);
+const getAllPosts = (callback) => {
+  Post.find({}).exec((err, results) => {
+    if (err) {
+      throw err;
+    } else {
+      callback(null, results);;
+    }
+  });
 }
 
 module.exports = getAllPosts;
