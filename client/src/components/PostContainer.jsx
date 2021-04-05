eba202e20cd7fbe4887cb3dcd0a08d594ebc4e5b
foodforcode <tsai.connie@yahoo.com>
2021-04-05 03:32:52 -0700

    Add component to display each post

--- client/src/components/PostContainer.jsx
+++ client/src/components/PostContainer.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import PostItem from './PostItem.jsx';
 
-function PostContainer (props) {
+function PostContainer({posts}) {
   return (
-    <h1>Here</h1>
+    <div>
+      <div className="row">
+          {posts.map((post) => {
+            return <PostItem key={post._id} name={post.name} date={post.date} body={post.body.slice(0, 70) + "..."} location={post.location} url={post.image}/>
+          })}
+      </div>
+    </div>
   )
 }
 
---
+++ client/src/components/PostItem.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+class PostItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayModal: false,
+    }
+    this.showModal = this.showModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  showModal (e) {
+    e.preventDefault();
+    console.log('show');
+    this.setState({
+      displayModal: true
+    })
+  }
+
+  closeModal () {
+    console.log('close');
+    this.setState({
+      displayModal: false
+    })
+  }
+
+  render() {
+    const divStyle = {
+      display: this.state.displayModal ? 'block' : 'none'
+    }
+    return (
+      <div className="card col-md-4 col-lg-4 col-sm-6" key={this.props.id}>
+        <h4 className="card-title post-name">{this.props.name}</h4>
+        <p>on {this.props.date} in {this.props.location}</p>
+        <div className="card-body">
+          {this.props.body}
+          <br />
+          {this.props.url ? <img class="img-item" width="100px" src={`${this.props.url}`} onClick={this.showModal}/>
+          : (null)}
+        </div>
+        {this.state.displayModal ?
+          <div className="Modal"
+            onClick={this.closeModal}
+            style={divStyle}
+          >
+            <div
+            className="Modal-Img fadeTransition"
+            onClick={e => e.stopPropagation()}
+            >
+           <img className="img-modal" max-width="200px" src={`${this.props.url}`} onClick={this.closeModal}/>
+            </div>
+          </div>
+          : (null)
+        }
+      </div>
+    )
+  }
+}
+
+export default PostItem;
